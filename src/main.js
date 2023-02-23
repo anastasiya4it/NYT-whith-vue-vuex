d22e291effb4c964c72de26c9327bf80ac239a7a
Bo136 <anastasiya4it@gmail.com>
2023-02-23 03:14:43 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from './App'
+import components from '@/components/UI'
+import router from './router/router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+components.forEach(item => {
+    app.component(item.name, item);
+});
+
+app
+.use(router) 
+.use(store)
+.mount('#app')
